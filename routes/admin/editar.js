4c83c75f7c22b149731bdd25fd5a3e66e34baa07
Agustín Iglesias <elagusiglesias1916@gmail.com>
2021-12-12 17:32:23 -0300

    Añadido el backend de editar

--- routes/admin/editar.js
+++ routes/admin/editar.js
@@ -5,11 +5,32 @@
 
 
     router.get('/', async(req, res, next)=>{
+        var id=articulo.id;
+        var titulo=articulo.titulo;
+        var cuerpo=articulo.cuerpo;
         res.render('admin/editar', {
             isEditar: true,
             layout: 'admin/layout',
-
+            id,
+            titulo,
+            cuerpo,
         });
     });
 
+    router.post('/', async(req, res, next)=>{
+        try{
+            var id=articulo.id;
+            var titulo=articulo.titulo;
+            var cuerpo=articulo.cuerpo;
+            await tisys.actualizarTI(id, titulo, cuerpo);
+            res.render('admin/editar', {
+                        isEditado: true,
+        });
+    }catch(error){
+        console.log(error);
+        res.render('admin/editar', {
+        isErrored: true,
+    });
+}
+});
     module.exports=router;
